a1279ee522c77f249adde3ce7cbe46e5954b13a2
Jonny Rose <jonny_rose44@yahoo.com>
2022-06-17 23:26:07 +0300

    added components

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Header() {
+    return (
+        <Navbar bg='light' sticky='top' className='Header'>
+            <Container>
+                <Navbar.Brand>Microblog</Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,19 @@
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+
+
+export default function Sidebar() {
+    return (
+        <Navbar sticky="top" className="flex-column Sidebar">
+            <Nav.Item>
+                <Nav.Link href="/">Feed</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/">Explore</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/">About</Nav.Link>
+            </Nav.Item>
+        </Navbar>
+    );
+}
