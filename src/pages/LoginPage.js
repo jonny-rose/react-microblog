82f344049cc8666efe1f770b396d1e101cc796b9
Jonny Rose <jonny_rose44@yahoo.com>
2022-06-26 15:17:18 +0300

    added api

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import Body from "../Components/Body";
+import Form from "react-bootstrap/Form";
+import InputField from "../Components/InputField";
+import Button from "react-bootstrap/Button";
 
 export default function LoginPage() {
+    const [formErrors, setFormErrors] = useState({});
+    
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        console.log('handle form here');
+    };
+
     return(
         <Body>
             <h1>Login</h1>
-            <p>TODO</p>
+            <Form onSubmit={onSubmit}>
+                <InputField name='username' label='Username or email address' error={formErrors.username} />
+                <InputField name='password' label='Password' type='password' error={formErrors.password} />
+                <Button variant='primary' type='submit'>Login</Button>
+            </Form>
         </Body>
-    )
+    );
 }
