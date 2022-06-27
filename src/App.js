a7ea2aac2637c5f5b11e0ea300f346acbef1093a
Jonny Rose <jonny_rose44@yahoo.com>
2022-06-27 16:10:38 +0300

    added forms, validation, flash messages

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import ExplorePage from "./pages/ExplorePage";
 import LoginPage from "./pages/LoginPage";
 import UserPage from "./pages/UserPage";
 import ApiProvider from "./contexts/ApiProvider";
+import RegistrationPage from "./pages/RegistrationPage";
+import FlashProvider from './contexts/FlashProvider';
 
 export default function App() {
 
   return (
     <Container fluid className="App">
       <BrowserRouter>
-        <ApiProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<FeedPage/>}/>
-            <Route path="/explore" element={<ExplorePage/>}/>
-            <Route path="/user/:username" element={<UserPage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="*" element={<Navigate to="/" />}/>
-          </Routes>
-        </ApiProvider>
+        <FlashProvider>
+          <ApiProvider>
+            <Header />
+            <Routes>
+              <Route path="/" element={<FeedPage/>}/>
+              <Route path="/explore" element={<ExplorePage/>}/>
+              <Route path="/user/:username" element={<UserPage/>}/>
+              <Route path="/login" element={<LoginPage/>}/>
+              <Route path='/register' element={<RegistrationPage />}/>
+              <Route path="*" element={<Navigate to="/" />}/>
+            </Routes>
+          </ApiProvider>
+        </FlashProvider>
       </BrowserRouter>
     </Container>
   );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Body from "../Components/Body";
 import Form from "react-bootstrap/Form";
 import InputField from "../Components/InputField";
@@ -6,20 +7,42 @@ import Button from "react-bootstrap/Button";
 
 export default function LoginPage() {
     const [formErrors, setFormErrors] = useState({});
+    const usernameField = useRef();
+    const passwordField = useRef();
+
+    useEffect(() => {
+        usernameField.current.focus();
+    }, []);
     
     const onSubmit = (ev) => {
         ev.preventDefault();
-        console.log('handle form here');
+        const username = usernameField.current.value;
+        const password = passwordField.current.value;
+
+        const errors = {};
+        if (!username) {
+            errors.username = "Username must not be empty.";
+        }
+        if (!password) {
+            errors.password = "Password must not be empty";
+        }
+        setFormErrors(errors);
+        if (Object.keys(errors).length > 0) {
+            return;
+        }
+
     };
 
     return(
         <Body>
             <h1>Login</h1>
             <Form onSubmit={onSubmit}>
-                <InputField name='username' label='Username or email address' error={formErrors.username} />
-                <InputField name='password' label='Password' type='password' error={formErrors.password} />
+                <InputField name='username' label='Username or email address' error={formErrors.username} fieldRef={usernameField} />
+                <InputField name='password' label='Password' type='password' error={formErrors.password} fieldRef={passwordField} />
                 <Button variant='primary' type='submit'>Login</Button>
             </Form>
+            <hr />
+            <p>Don't have an account? <Link to="/register">Register here</Link>!</p>
         </Body>
     );
 }
